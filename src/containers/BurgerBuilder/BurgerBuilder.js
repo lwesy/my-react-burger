0295f4ddc5de8a8e9040fcdde61cad08530a87d4
Liam <lw@esyoil.com>
2018-01-22 18:19:01 +0100

    Add price to orderSummary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -102,6 +102,7 @@ class BurgerBuilder extends Component {
             ingredients={this.state.ingredients}
             purchaseContinued={this.purchaseContinueHandler}
             purchaseCanceled={this.purchaseCancelHandler}
+            price={this.state.totalPrice}
           />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
