8257f68c99fdb74f430f43203748a74b7e6a0ac4
Liam <lw@esyoil.com>
2018-01-23 23:04:16 +0100

    Adding backdrop to the sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,33 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = (props) => (
-  <Fragment>
-    <SideDrawer />
-    <Toolbar />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  
+  sideDrawerCloseHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  }
 
-export default layout;
+  render() {
+    return (
+      <Fragment>
+        <SideDrawer 
+          closed={this.sideDrawerCloseHandler} 
+          open={this.state.showSideDrawer}
+        />
+        <Toolbar />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Fragment>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Fragment>
+      <Backdrop 
+        clicked={props.closed}
+        show={props.open}
+      />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Fragment>
   );
 };
 
