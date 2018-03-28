6a89d99a5b63ef72e3a216df773f4d8eaafe74a7
Liam <lw@esyoil.com>
2018-03-29 01:02:47 +0200

    Only show orders tab if user is logged in

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,10 +6,10 @@ import classes from './NavigationItems.css';
 const navigationItems = props => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
+    {props.isAuth ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
     {!props.isAuth
       ? <NavigationItem link="/auth">Authenticate</NavigationItem>
-      : <NavigationItem link="/logout">Logout</NavigationItem> }
+      : <NavigationItem link="/logout">Logout</NavigationItem>}
   </ul>
 );
 
