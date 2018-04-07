8acd1dc554c60fd18b4aa0e97c48a91c5469fd35
Liam <lw@esyoil.com>
2018-04-07 16:00:38 +0200

    add tests for auth reducer

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,32 @@
+import reducer from './auth';
+import * as types from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+  const initialState = {
+    token: null,
+    userId: null,
+    loading: false,
+    error: null,
+    authRedirectPath: '/'
+  };
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should store the token upon login', () => {
+    expect(reducer(initialState, {
+      type: types.AUTH_SUCCESS,
+      payload: {
+        token: 'some-token',
+        userId: 'some-id'
+      }
+    })).toEqual({
+      token: 'some-token',
+      userId: 'some-id',
+      loading: false,
+      error: null,
+      authRedirectPath: '/'
+    })
+  });
+});
